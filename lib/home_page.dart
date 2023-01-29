1d348105f6a95d8968f2b20e52c6f1c683dad9a1
Shudhansu Pandey <64619700+Shudh25@users.noreply.github.com>
2023-01-29 08:52:54 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String name = "Shudhansu";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(
+        child: Text("Ram Ram Ji by $name "),
+      )),
+      drawer: Drawer(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
